9f02c27fbe719a8f8e6ac014a8aafabfae77f577
Max Kuzmin <kazekage.kun@gmail.com>
2013-07-01 19:02:21 +0400

    some bug fixed

--- letters.py
+++ letters.py
@@ -1,22 +1,21 @@
-#-*- coding: utf8 -*-
-import sys 
+#-*- coding: utf-8 -*-
+import sys
 
-def counter(letters,text):
+def counter(text):
+    letters = {}
     for symbol in text:
-        if symbol in letters:
-            letters[symbol] += 1
-        else:
-            letters[symbol] = 1
+        if symbol.isalpha():
+            if symbol in letters:
+                letters[symbol] += 1
+            else:
+                letters[symbol] = 1
     return letters
 
 
 def main():
-    letters = {}
-    for text in sys.argv[1:]:
-        presorted = list(counter(letters,text).items())
-
-    presorted.sort(key = lambda item: item[1])
-    presorted.reverse()
+    text = ' '.join(s.decode("utf-8") for s in sys.argv[1:])
+    presorted = list(counter(text).items())
+    presorted.sort(key = lambda item: item[1], reverse=True)
     for k, v in presorted:
         print k, "=", v
 
